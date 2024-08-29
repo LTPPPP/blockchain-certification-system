ec8e8fc6821142330af51718499763dbd28d9ad9
LTP <lamphat279@gmail.com>
2024-08-29 16:21:27 +0700

    more git ignore

--- backend/services/APII.py
+++ backend/services/APII.py
@@ -1,4 +1,4 @@
-blockchain_address = ""
-smart_contract_address = ""
-account_address = ""
-private_key = ""
+blockchain_address = "https://mainnet.infura.io/v3/ce2682b6e9cb45399721e91a9de026c1"
+smart_contract_address = "0xf8e81D47203A594245E36C48e151709F0C19fBe8"
+account_address = "0x77F6651F36CeE5baf47380BC094091e4Fc37Ad38"
+private_key = "1bc1fcd40d7e6b3b3f71030c57dcab296ea575f5ff58526eb75ee1387aeb59b6"
